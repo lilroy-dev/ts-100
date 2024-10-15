3efa74e0ff72ae35533dfcefe2aafdf7ac847487
sv.konoplitskii <s.konoplitsky@yandex.com>
2024-10-15 10:06:56 +0300

    feat: type reverseString

--- task1/solution.js
+++ task1/solution.js
@@ -1,16 +1,3 @@
-const alternateCase = (str) => {
-    let result = '';
-    for (let i = 0; i < str.length; i++) {
-        if (i % 2 !== 0) {
-            result += str.charAt(i).toUpperCase();
-        } else {
-            result += str.charAt(i).toLowerCase();
-        }
-    }
-    return result;
+function reverseString(str:string):string {
+    return str.split('').reverse().join('');
 }
-
-
-
-console.log(alternateCase("javascript"));
-// Результат: "jAvAsCrIpT"
