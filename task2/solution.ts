39122968ab2bce0129e706229dbbef8fd70411f1
sv.konoplitskii <s.konoplitsky@yandex.com>
2024-10-19 11:08:05 +0300

    feat: type array with object

---
+++ task2/solution.ts
@@ -0,0 +1,11 @@
+interface Product1 {
+    name: string;
+    price: number;
+    quantity: number;
+}
+
+const products:Product1[] = [
+    { name: "Apple", price: 100, quantity: 2 },
+    { name: "Banana", price: 80, quantity: 5 },
+    { name: "Orange", price: 50, quantity: 8 }
+];
